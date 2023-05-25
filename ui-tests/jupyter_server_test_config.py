fa42808b23af08812668c9573561958709df32a3
martinRenou <martin.renou@gmail.com>
2023-05-25 09:31:53 +0200

    JupyterLab 4 update (#60)
    * JupyterLab 4 update
    
    * Update yarn.lock
    
    * More lock update
    
    * Iterate
    
    * Meh
    
    * Update yarnrc

--- ui-tests/jupyter_server_test_config.py
+++ ui-tests/jupyter_server_test_config.py
@@ -7,8 +7,6 @@ JavaScript objects through the global window variable.
 from jupyterlab.galata import configure_jupyter_server
 
 configure_jupyter_server(c)  # noqa
-# FIXME upstream
-c.LabApp.dev_mode = False  # noqa
 
 # Uncomment to set server log level to debug level
 # c.ServerApp.log_level = "DEBUG"
