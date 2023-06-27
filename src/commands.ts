14fb1373f1faae541db4e7745002093075d1f430
Duc Trung LE <leductrungxf@gmail.com>
2023-06-27 16:55:11 +0200

    Create launcher button

--- src/commands.ts
+++ src/commands.ts
@@ -2,6 +2,8 @@
  * The command IDs used by the control panel.
  */
 export namespace CommandIDs {
+  export const createNew = 'glue-control:new-session';
+
   export const new1DHistogram = 'glue-control:new-1d-histogram-viewer';
 
   export const new2DImage = 'glue-control:new-2d-image-viewer';
